3edda3ec59edd4fd167dfa6747111d172a2df194
xWTF <e@x.wtf>
2024-02-06 23:04:46 +0800

    install: implement REAL ssa / ass parsing

--- install.php
+++ install.php
@@ -78,11 +78,22 @@ function list_ass($dir, &$result)
         }
         if (is_dir($file)) {
             list_ass($dir . DIRECTORY_SEPARATOR . $file, $result);
-        } else if (str_ends_with(strtolower($file), '.ass')) {
+        } else if (str_ends_with(strtolower($file), '.ass') || str_ends_with(strtolower($file), '.ssa')) {
             $result[] = $dir . DIRECTORY_SEPARATOR . $file;
         }
     }
 }
+
+$_BOM_MAPPING = [
+    "\xEF\xBB\xBF" => 'UTF-8',
+    "\xFE\xFF" => 'UTF-16BE',
+    "\xFF\xFE" => 'UTF-16LE',
+    "\x00\x00\xFE\xFF" => 'UTF-32BE',
+    "\xFF\xFE\x00\x00" => 'UTF-32LE',
+    "\x2B\x2F\x76" => 'UTF-7',
+    "\xF7\x64\x4C" => 'UTF-1',
+    "\x84\x31\x95\x33" => 'GB18030',
+];
 
 $_INDEX = json_decode(file_get_contents(FONTS_STORAGE . DIRECTORY_SEPARATOR . 'index.json'), true);
 
@@ -94,48 +105,161 @@ foreach ($ass_files as $file) {
     echo ('Parsing subtitle: ' . $file . PHP_EOL);
     $data = file_get_contents($file);
 
-    // Try to detect encoding
-    $encoding = mb_detect_encoding($data);
-    if ($encoding != 'UTF-8') {
-        if (empty($encoding)) {
-            foreach (['UTF-16LE', 'UTF-16BE'] as $c) {
-                $try = mb_convert_encoding($data, 'UTF-8', $c);
-                if (stripos($try, '[Script Info]') !== false) {
-                    $encoding = $c;
-                    $data = $try;
-                    break;
-                }
+    // Check for BOMs first
+    $encoding = '';
+    foreach ($_BOM_MAPPING as $bom => $e) {
+        if (str_starts_with($data, $bom)) {
+            $encoding = $e;
+            $data = substr($data, strlen($bom));
+
+            if ($e !== 'UTF-8') {
+                $data = mb_convert_encoding($data, 'UTF-8', $e);
             }
-        } else if ($encoding == 'CP936') {
-            $data = mb_convert_encoding($data, 'UTF-8', $encoding);
+            break;
         }
-        echo ('	unusual encoding: ' . $encoding . PHP_EOL);
+    }
+
+    // BOM not present, detection required
+    if (empty($encoding) && !mb_check_encoding($data, 'UTF-8')) {
+        // Test UTF-16 first
+        foreach (['UTF-16LE', 'UTF-16BE'] as $e) {
+            $try = mb_convert_encoding($data, 'UTF-8', $e);
+            if (str_starts_with($try, '[Script Info]')) {
+                $encoding = $e;
+                $data = $try;
+                break;
+            }
+        }
 
         if (empty($encoding)) {
-            die('	unknown encoding');
+            if (empty($encoding = mb_detect_encoding($data, [
+                'CP936', 'BIG-5', 'SJIS',
+            ], true))) {
+                die('	unknown encoding');
+            }
+            $data = mb_convert_encoding($data, 'UTF-8', $encoding);
+            echo ('	unusual encoding: ' . $encoding . PHP_EOL);
         }
     }
 
-    // Find styles sections
-    if (!preg_match_all('/\[V4\+ Styles\]\s*\n([^[]+)\n\[/im', str_replace("\r", "\n", $data), $v4_styles)) {
-        die('	bad ass file: V4+ styles not found');
+    // Parse SSA v4.00 / ASS
+    if (!str_starts_with($data, '[Script Info]')) {
+        die('	malformed file: not start with [Script Info]');
     }
-    $v4_styles = implode("\n", $v4_styles[1]);
 
-    // Parse fonts
     $fonts = [];
-    foreach (explode("\n", $v4_styles) as $line) {
+    $embedded_fonts = [];
+
+    $styles = [];
+    $current_section = '';
+    $current_format = null;
+    foreach (explode("\n", str_replace("\r", "\n", $data)) as $line) {
         $line = trim($line);
-        if ($line == '' || $line[0] == ';') {
+        if ($line == '' || $line[0] == ';' || str_starts_with($line, '!:')) {
             continue;
         }
-        if (str_starts_with($line, 'Style: ')) {
-            $data = explode(',', $line);
-            $fonts[] = [trim($data[1], " \t\n\r\0\x0B@"), boolval($data[7]), boolval($data[8])];
-        } else if (!str_starts_with(strtolower(str_replace(' ', '', $line)), 'format:name,fontname,fontsize,primarycolour,secondarycolour,outlinecolour,backcolour,bold,italic')) {
-            die('	bad format or line: ' . $line);
+        if ($line[0] == '[') {
+            $current_section = strtolower($line);
+            $current_format = null;
+            continue;
+        }
+
+        if (!in_array($current_section, ['[v4 styles]', '[v4+ styles]', '[events]', '[fonts]'])) {
+            continue;
+        }
+
+        $l1 = $line;
+        $line = explode(':', $line, 2);
+        if (count($line) !== 2) {
+            die('	malformed file: unrecognized line');
+        }
+        [$type, $line] = $line;
+        $line = array_map('trim', explode(',', $line, $current_format === null ? PHP_INT_MAX : count($current_format)));
+
+        if ($type === 'Format') {
+            $current_format = $line;
+            continue;
+        } else if ($current_format === null) {
+            die('	malformed file: undefined format');
+        }
+        if (count($current_format) !== count($line)) {
+            die('	malformed file: format mismatch');
+        }
+        $line = array_combine($current_format, $line);
+
+        switch ($current_section) {
+            case '[v4 styles]':
+            case '[v4+ styles]':
+                if ($type !== 'Style') {
+                    die('	malformed file: unrecognized type ' . $type . ' in ' . $current_section);
+                }
+                if (!isset($line['Name'], $line['Fontname'], $line['Bold'], $line['Italic'])) {
+                    die('	malformed file: Name / Fontname / Bold / Italic not found');
+                }
+                $fonts[] = $styles[$line['Name']] = [
+                    trim($line['Fontname'], '@'),
+                    boolval($line['Bold']),
+                    boolval($line['Italic']),
+                ];
+                break;
+            case '[events]':
+                if ($type === 'Comment') {
+                    break;
+                } else if ($type !== 'Dialogue') {
+                    // Will die when Picture / Sound / Movie / Command found
+                    // Nobody use these anyway :)
+                    die('	malformed file: unrecognized type ' . $type . ' in ' . $current_section);
+                }
+                if (!isset($line['Text'], $line['Style'])) {
+                    die('	malformed file: Text / Style not found');
+                }
+
+                $style = $styles[$line['Style']] ?? $styles['Default'] ?? null;
+                if ($style === null) {
+                    // style not found and no default style, the player will use default font
+                    break;
+                }
+                $style_fn = $style[0];
+
+                if (preg_match_all('/{(\\\\[^}]+)}/', $line['Text'], $matches) === false) {
+                    die('	malformed file: unable to parse override sequences');
+                }
+                foreach ($matches[1] as $match) {
+                    $match = substr($match, 1);
+                    foreach (explode('\\', $match) as $m) {
+                        if (strlen($m) < 2) {
+                            continue;
+                        }
+                        switch (substr($m, 0, 2)) {
+                            case 'b1':
+                                $style[1] = true;
+                                break;
+                            case 'b0':
+                                $style[1] = false;
+                                break;
+                            case 'i1':
+                                $style[2] = true;
+                                break;
+                            case 'i0':
+                                $style[2] = false;
+                                break;
+                            case 'fn':
+                                $style[0] = $m === 'fn' ? $style_fn : trim(substr($m, 2), '@');
+                                break;
+                            default:
+                                continue 2;
+                        }
+                        $fonts[] = $style;
+                    }
+                }
+                break;
+            case '[fonts]':
+                // TODO: we can't parse the font file yet
+                break;
         }
     }
+
+    // Filter fonts
     $fonts = array_unique($fonts, SORT_REGULAR);
     if (count($fonts) == 0) {
         die('	no fonts defined!');
